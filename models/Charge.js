f89c049b1484f189c197b5c74f7663b2c150842b
joanbrown1 <joannabrown833@gmail.com>
2024-04-23 18:26:28 +0100

    charge number

--- models/Charge.js
+++ models/Charge.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const chargeSchema = new mongoose.Schema({
     charge: {
-        type: String,
+        type: Number,
         required: true
     },
     date: {
