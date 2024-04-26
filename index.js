8a8110b5573bddc95f822fa74957360e7e2feae5
joanbrown1 <joannabrown833@gmail.com>
2024-04-26 12:41:53 +0100

    admin api

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const connection = require("./db");
 const { Transaction } = require("./models/Transaction");
 const { Charge } = require("./models/Charge");
 const { Discount } = require("./models/Discount");
+const { Admin } = require("./models/Admin");
 const userRoutes = require("./routes/Users");
 const authUserRoutes = require("./routes/authUser");
 const updateUserRoutes = require("./routes/updateUser");
+const updateAdminRoutes = require("./routes/updateAdmin");
 const { User } = require("./models/user");
 
 app.use(express.json());
@@ -211,6 +213,34 @@ app.post("/discounts/code", async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 });
+
+// API for Admin
+
+app.post('/admin', async (req, res) => {
+  try {
+      await Admin.create(req.body);
+      res.status(200).json({message: "Admin created"});
+  } catch (error) {
+      console.error(error.message);
+      res.status(500).json({ message: error.message });
+  }
+});
+
+app.get('/admins', async (req, res) => {
+  try {
+      const admins = await Admin.find({});
+
+      // Sort admins by timestamp in ascending order (earliest first)
+      admins.reverse();
+
+      res.status(200).json(admins);
+  } catch (error) {
+      console.error(error.message);
+      res.status(500).json({ message: error.message });
+  }
+});
+
+app.use("/update/admin", updateAdminRoutes);
 
 //app.listen();
 
---
+++ routes/updateAdmin.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const { Admin } = require("../models/Admin");
+
+// PUT endpoint to update admin privilages
+router.put("/", async (req, res) => {
+    try {
+        // Find the admin by email
+        const admin = await Admin.findOne({ email: req.body.email });
+        if (!admin) return res.status(404).send({ message: "Admin not found" });
+
+        // Update admin data
+        if (req.body.privilage) admin.privilage = req.body.privilage;
+        await admin.save();
+
+        res.status(200).send({ message: "Admin privilages updated successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Internal Server Error" });
+    }
+});
+
+module.exports = router;
