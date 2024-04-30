fb316c2b3c2e920dcfb2cef016b89e1e0c169dc3
joanbrown1 <joannabrown833@gmail.com>
2024-04-30 22:16:04 +0100

    password checker out

--- .history/routes/authUser_20240430221244.js
+++ .history/routes/authUser_20240430221244.js
@@ -5,9 +5,9 @@ const bcrypt = require("bcrypt");
 
 router.post("/", async (req, res) => {
 	try {
-		// const { error } = validate(req.body);
-		// if (error)
-		// 	return res.status(400).send({ message: error.details[0].message });
+		const { error } = validate(req.body);
+		if (error)
+			return res.status(400).send({ message: error.details[0].message });
 
 		const user = await User.findOne({ email: req.body.email });
 		if (!user)
