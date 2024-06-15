46e339d6ba064e59265fa659690659d969d051de
joanbrown1 <joannabrown833@gmail.com>
2024-06-15 07:56:34 +0100

    faq api

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const puppeteer = require('puppeteer');
 const nodemailer = require('nodemailer');
 const { Conversation } = require("./models/Conversation");
 const { Messsage } = require("./models/Message");
+const { Faq } = require("./models/Faq");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -663,6 +664,32 @@ app.post("/password", async(req, res) => {
         res.status(200).json({ message: 'Email sent' });
       }
   });
+});
+
+//API for faq
+
+app.post('/faq', async (req, res) => {
+  try {
+      await Faq.create(req.body);
+      res.status(200).json({message: "Faq created"});
+  } catch (error) {
+      console.error(error.message);
+      res.status(500).json({ message: error.message });
+  }
+});
+
+app.get('/faqs', async (req, res) => {
+  try {
+      const faqs = await Faq.find({});
+
+      // Sort discounts by timestamp in ascending order (earliest first)
+      faqs.reverse();
+
+      res.status(200).json(faqs);
+  } catch (error) {
+      console.error(error.message);
+      res.status(500).json({ message: error.message });
+  }
 });
 
 //app.listen();
---
+++ models/Faq.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+
+const faqSchema = new mongoose.Schema({
+    question: {
+        type: String,
+        required: true
+    },
+    answer: {
+        type: String,
+        required: true
+    },
+});
+
+const Faq = mongoose.model("Faq", faqSchema);
+
+module.exports = { Faq };
+
+
+
