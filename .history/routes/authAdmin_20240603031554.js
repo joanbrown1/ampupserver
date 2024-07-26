4f2cb1f5b958b821b02e50ff9b00efe5124f6f94
joanbrown1 <joannabrown833@gmail.com>
2024-07-26 02:50:43 +0100

    mail changes

--- .history/routes/authAdmin_20240603031554.js
+++ .history/routes/authAdmin_20240603031554.js
@@ -20,7 +20,8 @@ router.post("/", async (req, res) => {
 		if (!validPassword)
 			return res.status(401).send({ message: "Invalid Email or Password" });
 
-		res.status(200).send({ data: { admin }, message: "logged in successfully" });
+		const token = admin.generateAuthToken();
+		res.status(200).send({ data: { token, admin }, message: "logged in successfully" });
 	} catch (error) {
 		res.status(500).send({ message: "Internal Server Error", error: error.message });
 	}
